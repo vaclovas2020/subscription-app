5021bd5aaee01ff337f6040f050b993c1cc738c3
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2021-10-22 23:31:19 +0000

    fix load problems

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,8 @@
 FROM ruby:2.7.0
-WORKDIR /ruby
-ADD ./best-subscription-app ./app
-RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
-RUN cd app
+WORKDIR /ruby/app
+ADD ./best-subscription-app ./
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client npm 
+RUN npm install -g yarn
 RUN bundle install
-RUN rails install
-RUN rails generate shopify_app
-RUN bundle install
-RUN rails generate graphql:install
-RUN bundle install
-RUN rails db:migrate
-RUN rails webpacker:install
-RUN rails webpacker:install:react
-RUN rails generate react:install
-ENTRYPOINT ["rails", "server", "-b", "subscription_app"]
+ENTRYPOINT ["rails", "server", "-b", "0.0.0.0", "-e", "development"]
 EXPOSE 3000
--- best-subscription-app/app/javascript/components/TestData.js
+++ best-subscription-app/app/javascript/components/TestData.js
@@ -1,24 +1,19 @@
 import React from "react"
 import { gql, useQuery } from '@apollo/client';
 const TEST_QUERY = gql`query { testField }`;
-class TestData extends React.Component {
-  render () {
-    const {loading, error, data} = useQuery(TEST_QUERY);
-
-    if (loading) {
-      return (
-        <div>Loading</div>
-      );
-    } else if (error) {
-      return (
-        <div>Something went wrong!</div>
-      );
-    } else {
-      return (
-        <p>{data.testField}</p>
-      );
-    }
+export default function TestData(){
+  const { loading, error, data } = useQuery(TEST_QUERY);
+  if (loading) {
+    return (
+      <div>Loading</div>
+    );
+  } else if (error) {
+    return (
+      <div>Something went wrong!</div>
+    );
+  } else {
+    return (
+      <p>{data.testField}</p>
+    );
   }
 }
-
-export default TestData
