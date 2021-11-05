207c63a85c8bbc6a0d963ceddfc0101ba347f175
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2021-11-05 21:44:28 +0200

    update app scope

--- best-subscription-app/config/initializers/shopify_app.rb
+++ best-subscription-app/config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "Best Subscription App"
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products,read_customer_payment_methods,write_own_subscription_contracts" # Consult this page for more scope options:
                                   # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
